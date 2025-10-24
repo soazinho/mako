58ea4721595ee4de3941f530bb8d7ad606c38426
soazinho <hugues.carlos.soares@gmail.com>
2025-10-24 03:22:40 -0400

    use loader data

--- app/routes/protected.tsx
+++ app/routes/protected.tsx
@@ -14,6 +14,7 @@ export default function Protected({ loaderData }: Route.ComponentProps) {
 	return (
 		<div>
 			<h1>Welcome {loaderData?.email}!</h1>
+			<h2>Your email is {loaderData?.name}.</h2>
 		</div>
 	);
 }
