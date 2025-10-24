843e888686a21d90bdc215ac941e0d4a249af058
soazinho <hugues.carlos.soares@gmail.com>
2025-10-24 16:09:02 -0400

    logout form

--- app/routes/protected.tsx
+++ app/routes/protected.tsx
@@ -1,7 +1,18 @@
+import { redirect } from "react-router";
+import { Button } from "~/components/ui/button";
 import { userContext } from "~/contexts/context";
 import { authMiddleware } from "~/middlewares/auth";
-
+import { destroySession, getSession } from "~/server/session.server";
 import type { Route } from "./+types/protected";
+
+export async function action({ request }: Route.ActionArgs) {
+	const session = await getSession(request.headers.get("Cookie"));
+	return redirect("/login", {
+		headers: {
+			"Set-Cookie": await destroySession(session),
+		},
+	});
+}
 
 export const middleware = [authMiddleware];
 
@@ -15,6 +26,9 @@ export default function Protected({ loaderData }: Route.ComponentProps) {
 		<div>
 			<h1>Welcome {loaderData?.email}!</h1>
 			<h2>Your email is {loaderData?.name}.</h2>
+			<form method="post">
+				<Button>Logout</Button>
+			</form>
 		</div>
 	);
 }
